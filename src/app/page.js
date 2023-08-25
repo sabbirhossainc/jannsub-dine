faa1fa4d12a8b33ea2794bebd5636b31e3c7e91b
sabbirhossainc <shr472978@gmail.com>
2023-08-26 00:25:53 +0600

    first deploy hook

--- src/app/page.js
+++ src/app/page.js
@@ -3,7 +3,6 @@ import Image from 'next/image'
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center p-24 gap-0">
-
       <div className="grid text-center lg:max-w-5xl lg:w-full lg:mb-0">  
           <h1 className={`mb-4 text-6xl font-bold`}>
             Jannsub Dine
@@ -12,7 +11,7 @@ export default function Home() {
 
       <div className="grid text-center lg:max-w-5xl lg:w-full lg:mb-0">  
           <h2 className={`mb-3 text-4xl font-semibold`}>
-            Comming soon...
+            Comming soon ...
           </h2>
       </div>
     </main>
